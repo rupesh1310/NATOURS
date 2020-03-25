3ee0fedd373abd05fdbef63db3d15fc0839608a5
rupesh1310 <a.rupesh66@gmail.com>
2020-03-26 03:34:41 +0530

    better file structuring v3

--- app.js
+++ app.js
@@ -26,8 +26,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) START THE SERVER
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
