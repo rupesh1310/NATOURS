a099390232a1f8b35c085acdbb8a7b2d7ed46af5
rupesh1310 <a.rupesh66@gmail.com>
2020-03-25 21:48:03 +0530

    handling patch requests

--- app.js
+++ app.js
@@ -68,6 +68,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.param.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
