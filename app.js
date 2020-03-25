d57df848a74d4417a41d6e04d10119c8d6a68885
rupesh1310 <a.rupesh66@gmail.com>
2020-03-26 00:12:12 +0530

    creating own middlewares

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
 const express = require('express');
-const app = express();
 
+const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours
@@ -99,6 +111,7 @@ app
   .route('/api/v1/tours')
   .get(getAllTours)
   .post(createTour);
+
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
