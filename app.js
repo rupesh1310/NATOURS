d3415fdbb5960ad54cc6a19ef719b1f3d42346b5
rupesh1310 <a.rupesh66@gmail.com>
2020-03-26 00:29:16 +0530

    adding 3rd party middlewares

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +21,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE-HANDLERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -106,6 +112,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 //Better Way
 app
   .route('/api/v1/tours')
@@ -117,6 +124,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START THE SERVER
 
 const port = 3000;
 app.listen(port, () => {
