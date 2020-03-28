ce9ff8f694806c52bd7383ffc93fa8e321e87874
rupesh1310 <a.rupesh66@gmail.com>
2020-03-28 23:45:19 +0530

    better limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     //Execute the query
     const tours = await query;
 
