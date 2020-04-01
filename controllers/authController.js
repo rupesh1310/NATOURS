00fbcbe236e3c0f18f97fe3dfbf1408703b1b9e2
rupesh1310 <a.rupesh66@gmail.com>
2020-04-02 05:14:26 +0530

    protecting routes of tour v1

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,3 +45,28 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1)Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' '[1]);
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  //2) Verification token
+
+  //3) Check if user still exists
+
+  //4) Check if the user changed password after the token was issued
+
+  next();
+});
