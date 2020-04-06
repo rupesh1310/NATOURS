09ea2f465f819492d116b1a66e742b78c3367977
rupesh1310 <a.rupesh66@gmail.com>
2020-04-07 00:42:04 +0530

    :sparkles: virtual populate tours & reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -110,6 +110,7 @@ const tourSchema = new mongoose.Schema(
       },
     ],
   },
+
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -118,6 +119,13 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
