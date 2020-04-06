4dce4917d5e272be8b46a6fcf6ba3af4e72b0751
rupesh1310 <a.rupesh66@gmail.com>
2020-04-06 21:24:46 +0530

    :rotating_light: modelling tour guides embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -102,6 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -119,6 +121,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
 //   next();
