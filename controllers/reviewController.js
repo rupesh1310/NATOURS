7b50a4fdfc7b732462139b8a14f18b840bf247d1
rupesh1310 <a.rupesh66@gmail.com>
2020-04-07 03:39:36 +0530

    :tada: factory function reading v2

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,7 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-};
+// const catchAsync = require('./../utils/catchAsync');
 
 exports.setTourUserIds = (req, res, next) => {
   // ALlow nested routes
@@ -24,6 +10,7 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
--- controllers/userController.js
+++ controllers/userController.js
@@ -11,19 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTS password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -61,14 +48,15 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = factory.updateOne(User);
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined! Please use /signup instead',
   });
 };
+
+exports.getUser = factory.updateOne(User);
+exports.getAllUsers = factory.getAll(User);
 
 // Do not upadte passwords with this!
 exports.updateUser = factory.updateOne(User);
