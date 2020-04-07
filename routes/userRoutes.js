0a0efdbb49eb85b96d6a5f6e3d0ce5b93c6cd2c6
rupesh1310 <a.rupesh66@gmail.com>
2020-04-07 08:18:43 +0530

    :tada: adding missing authentication & authorization v2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,14 +9,15 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+// Protect all routes after this middleware
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-
 router.get('/me', userController.getMe, userController.getUser);
-
 router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
