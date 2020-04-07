55dbc30d764fa0bc704e1f37fb0470c9e6f7c038
rupesh1310 <a.rupesh66@gmail.com>
2020-04-08 03:29:55 +0530

    :tada: preventing duplicate reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -36,6 +36,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, //4.66667, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
