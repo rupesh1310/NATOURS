470dce1b55283033c617ac4b91a026895b9d0fc8
rupesh1310 <a.rupesh66@gmail.com>
2020-04-10 03:49:06 +0530

    :green_heart: logging in users with API 3.1

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
+import axios from 'axios';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -21,10 +22,3 @@ const login = async (email, password) => {
     alert(err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
