b28d5f982506dc39c6981e95c3551ef19156e22d
rupesh1310 <a.rupesh66@gmail.com>
2020-04-10 08:20:30 +0530

    :green_heart: updating user data

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8640,7 +8640,7 @@ var _login = require("./login");
 
 // DOM ELEMENTS
 var mapBox = document.getElementById('map');
-var loginForm = document.querySelector('.form');
+var loginForm = document.querySelector('.form--login');
 var logOutBtn = document.querySelector('.nav__el--logout'); // DELEGATION
 
 if (mapBox) {
@@ -8683,7 +8683,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51318" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63460" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
